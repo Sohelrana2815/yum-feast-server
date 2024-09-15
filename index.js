22fdb6b73df5c803ce46648008f966f443c7d1e4
Sohelrana2815 <sohel152302@gmail.com>
2024-09-15 14:17:25 +0600

    menus api : post, get, patch and delete

--- index.js
+++ index.js
@@ -156,10 +156,42 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/menus/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await menuCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.patch("/menus/:id", async (req, res) => {
+      const item = req.body;
+      const id = req.params.id;
+
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          name: item.name,
+          category: item.category,
+          price: item.price,
+          image: item.image,
+        },
+      };
+
+      const result = await menuCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
+
     app.post("/menus", verifyToken, verifyAdmin, async (req, res) => {
       const item = req.body;
 
       const result = await menuCollection.insertOne(item);
+      res.send(result);
+    });
+
+    app.delete("/menus/:id", verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await menuCollection.deleteOne(query);
       res.send(result);
     });
 
